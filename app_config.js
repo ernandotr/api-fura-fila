83ee564ead17d6c67c55b886745c0085e71340ee
Ernando Rezende <ernando@MacBook-Pro-de-Ernando.local>
2016-09-11 14:47:20 -0300

    correcao na config do servidor acessos .

--- app_config.js
+++ app_config.js
@@ -8,7 +8,7 @@ app.set('port', (process.env.PORT || 5000));
 
 var allowCors = function(req, res, next){
 
-	res.header('Acces-Control-Allow-Origin', '127.0.0.1:5000');
+	res.header('Acces-Control-Allow-Origin', '*');
 	res.header('Acces-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 	res.header('Acces-Control-Allow-Headers', 'Content-Type');
 	res.header('Acces-Control-Allow-Credentials', 'true');
